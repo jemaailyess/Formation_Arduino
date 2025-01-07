d3195b357385676c01e582aa4bb19eaedeff9fe7
jemaailyess <djemaailyess210@gmail.com>
2025-01-07 11:40:29 +0100

    Comprendre la fonction "analogWrite"

---
+++ Fct_analogWrite/Fct_analogWrite.ino
@@ -0,0 +1,12 @@
+int ledPin = 3;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  analogWrite(ledPin, 125);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
