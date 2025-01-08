64680487beca41f4ac9f3d9d8b24c9628c42ec98
jemaailyess <djemaailyess210@gmail.com>
2025-01-08 23:08:20 +0100

    Temperature and humidity sensor

---
+++ DHT11/DHT11.ino
@@ -0,0 +1,22 @@
+#include "DHT.h"
+DHT dht (2, DHT11);
+float temp;
+float humidity;
+void setup() {
+  // put your setup code here, to run once:
+dht.begin();
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+delay(2000);
+temp = dht.readTemperature();
+humidity = dht.readHumidity();
+Serial.print("Temperature: ");
+Serial.print(temp);
+Serial.print(" C");
+Serial.print("Humidity: ");
+Serial.print(humidity);
+Serial.print(" %");
+}
